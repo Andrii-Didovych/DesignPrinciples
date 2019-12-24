304fdab1e287ae382d70bb20833e16aafa1452e9
Andrii Didovych <andrii.didovych@sombrainc.com>
2019-12-24 10:35:25 +0200

    Initial commit

---
+++ src/isp/Document.java
@@ -0,0 +1,143 @@
+package isp;
+
+/*
+Отже, даний принцип означає, що занадто «товсті» інтерфейси необхідно розділяти на менші та специфічні, щоб їх клієнти
+знали лише про ті методи, що необхідні для них у роботі. Як результат, при зміні певного функціоналу, незмінними мають
+лишитися ті класи, що не використовують його. Тобто виконання цього принципу допомагає системі залишатися гнучкою при
+ внесенні до неї змін та лишатися простою для рефакторингу.
+ */
+
+public class Document {}
+
+interface Machine {
+    void print(Document document);
+    void fax(Document document) throws Exception;
+    void scan(Document document);
+}
+
+class MultiFuntionalPrinter implements Machine {
+
+    @Override
+    public void print(Document document) {
+        //
+    }
+
+    @Override
+    public void fax(Document document) {
+        //
+    }
+
+    @Override
+    public void scan(Document document) {
+        //
+    }
+}
+
+class OldFachionedPrinter implements Machine {
+
+    @Override
+    public void print(Document document) {
+        //
+    }
+
+    @Override
+    public void fax(Document document) throws Exception {
+        throw new Exception();
+    }
+
+    @Override
+    public void scan(Document document) {
+        //
+    }
+}
+
+// YAGNI = You Ain't Going to Need It
+
+interface Printer {
+    void print(Document document);
+}
+
+interface Scanner {
+    void scan(Document document);
+}
+
+class JustAPrinter implements Printer {
+
+    @Override
+    public void print(Document document) {
+        System.out.println("Print documents");
+    }
+}
+
+class JustAScanner implements Scanner {
+
+    @Override
+    public void scan(Document document) {
+        System.out.println("Scan document");
+    }
+}
+
+class PhotoCopier implements Printer, Scanner {
+
+    private Printer printer;
+    private Scanner scanner;
+
+    public PhotoCopier(Printer printer, Scanner scanner) {
+        this.printer = printer;
+        this.scanner = scanner;
+    }
+
+    @Override
+    public void print(Document document) {
+        printer.print(document);
+    }
+
+    @Override
+    public void scan(Document document) {
+        scanner.scan(document);
+    }
+}
+
+interface MultiFunctionDevice extends Printer, Scanner {}
+
+class MultiFunctionDeviceImpl implements MultiFunctionDevice {
+
+    private Printer printer;
+    private Scanner scanner;
+
+    public MultiFunctionDeviceImpl(Printer printer, Scanner scanner) {
+        this.printer = printer;
+        this.scanner = scanner;
+    }
+
+    @Override
+    public void print(Document document) {
+        printer.print(document);
+    }
+
+    @Override
+    public void scan(Document document) {
+        scanner.scan(document);
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        Document document = new Document();
+
+        JustAScanner justAScanner = new JustAScanner();
+        justAScanner.scan(document);
+
+        JustAPrinter justAPrinter = new JustAPrinter();
+        justAPrinter.print(document);
+
+        MultiFunctionDeviceImpl multiFunctionDevice = new MultiFunctionDeviceImpl(new JustAPrinter(), new JustAScanner());
+        multiFunctionDevice.scan(document);
+        multiFunctionDevice.print(document);
+
+        PhotoCopier photoCopier = new PhotoCopier(new JustAPrinter(), new JustAScanner());
+        photoCopier.scan(document);
+        photoCopier.print(document);
+    }
+}
+
---
+++ src/lsp/Rectangle.java
@@ -0,0 +1,109 @@
+package lsp;
+
+/*
+    якщо S підтип T, тоді об'єкти типу T в програмі можуть бути заміщені об'єктами
+    типу S без будь-яких змін бажаних властивостей цієї програми
+ */
+
+
+public class Rectangle {
+
+    protected int width, height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getArea() {
+        return width * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    public boolean isSquare() {
+        return height == width;
+    }
+}
+
+class Square extends Rectangle {
+
+    public Square() {
+    }
+
+    public Square(int size) {
+        width = height = size;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+        super.setWidth(height);
+        super.setHeight(height);
+    }
+}
+
+class RectangleFactory {
+
+    public static Rectangle newRectangle(int height, int width) {
+        return new Rectangle(width, height);
+    }
+
+    public static Rectangle newSquare(int side) {
+        return new Rectangle(side, side);
+    }
+
+}
+
+class Demo {
+
+    static void useIt(Rectangle rectangle) {
+        int width = rectangle.getWidth();
+        rectangle.setHeight(10);
+        //area = width *10
+        System.out.println(
+                "Expected area of " + (width * 10) + ", got " + rectangle.getArea()
+        );
+    }
+
+    public static void main(String[] args) {
+        Rectangle rc = new Rectangle(2, 3);
+        useIt(rc);
+
+        Rectangle square = new Square(5);
+        useIt(square);
+
+        Rectangle rectangle = RectangleFactory.newSquare(5);
+        useIt(rectangle);
+    }
+}
